718392e37f7a5efb8baedffa52776131ae997e00
Selena <selena@hawaii-macbook-air.local>
2018-08-18 23:51:08 +0800

    added slider and segmented control.

--- AppDevTsumshopping/ViewController.swift
+++ AppDevTsumshopping/ViewController.swift
@@ -12,10 +12,14 @@ var goal: Date!
 var now: Date!
 
 class ViewController: UIViewController {
-    @IBOutlet weak var goalTextField: UITextField!
-    @IBOutlet weak var myDatePicker: UIDatePicker!
-    @IBOutlet weak var nowTextField: UITextField!
+    @IBOutlet weak var goalTextField: UITextField! //開幕時間的文字顯示
+    @IBOutlet weak var myDatePicker: UIDatePicker! //時間選擇器
+    @IBOutlet weak var nowTextField: UITextField! //今天的文字顯示
     @IBOutlet weak var intervalTextField: UITextField!
+    @IBOutlet weak var myImageView1: UIImageView! //mySegmentedControl對應的那張圖片 定義
+    @IBOutlet weak var sliderLabel: UILabel! //被Slider調整字體大小的文字
+    @IBOutlet weak var fontSizeSlider: UISlider! //Slider調整字體大小
+    
     
     override func viewDidLoad() {
         myDatePicker.minimumDate = Date() //防止時間變成負數
@@ -24,6 +28,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    //以下時間倒數器
     @IBAction func myDatePickerAction(_ sender: UIDatePicker) {
         goal = sender.date // myDatePicker選擇的日子
         let now = Date()
@@ -37,5 +42,23 @@ class ViewController: UIViewController {
         intervalTextField.text = ("\(integerinterval)")
         print(integerinterval) //輸出計算結果
     }
-
+    
+    //以下是mySegmentedControl的程式碼
+    @IBAction func mySegmentedControl(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 { //判斷點選了哪個segmented
+            myImageView1.image = UIImage(named: "images-4") //顯示 Mickey
+        }else {
+            myImageView1.image = UIImage(named: "images-2") //顯示 Partners
+        }
+    }
+    
+    //以下是用 Slider 調整 font Size 的程式碼
+ 
+    @IBAction func sliderAction(_ sender: UISlider) {
+        print("slider value \(fontSizeSlider.value)")
+        self.sliderLabel.font = UIFont.systemFont(ofSize: CGFloat(fontSizeSlider.value))
+        
+    }
 }
+    
+
